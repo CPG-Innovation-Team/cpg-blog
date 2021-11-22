bbf536011bb97fc9f2d4874d5511611452fa467f
chenxi <xichencx@163.com>
2021-11-22 12:21:49 +0800

    update config

--- cmd/cpg/start.sh
+++ cmd/cpg/start.sh
@@ -1,3 +1,14 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2009
+# shellcheck disable=SC2126
+
+num=$(ps -elf | grep main | wc -l)
+if [ "${num}" -gt 1 ]; then
+killall main
+else
+echo "start deploy"
+fi
+
 cd /data/cpg-blog/cmd/cpg/ && go build main.go
 nohup /data/cpg-blog/cmd/cpg/main > /data/cpg-blog/cmd/cpg/gin.log 2>&1 &
 ps -elf | grep main
