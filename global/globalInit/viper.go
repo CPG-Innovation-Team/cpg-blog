4858cc58f97fcfb005f6131365d4bbfdae7c5628
chenxi <xichencx@163.com>
2022-06-29 18:53:53 +0800

    feature: viper remote connection nacos

--- global/globalInit/viper.go
+++ global/globalInit/viper.go
@@ -4,19 +4,20 @@ import (
 	"flag"
 	"fmt"
 	"github.com/spf13/viper"
+	_ "github.com/spf13/viper/remote"
 	"math/rand"
 	"sync"
 	"time"
 )
 
 var (
-	once = new(sync.Once)
-	config = flag.String("config", "config", "配置文件名称，默认 config")
+	once       = new(sync.Once)
+	config     = flag.String("config", "config", "配置文件名称，默认 config")
 	LocalDebug = flag.Bool("local", false, "本地启动，默认 false")
-	Base    int
+	Base       int
 )
 
-func init()  {
+func init() {
 	ViperInit()
 }
 func ViperInit() {
@@ -28,17 +29,26 @@ func ViperInit() {
 		rand.Seed(time.Now().UnixNano())
 
 		//配置文件名称
-		fmt.Print(*config)
+		fmt.Println("配置文件名称：", *config)
 		viper.SetConfigName(*config)
-		viper.SetConfigType("toml")
+		viper.SetConfigType("yaml")
 		//配置文件查找路径
 		viper.AddConfigPath("/etc/cpg-blog")
 		viper.AddConfigPath("$HOME/.cpg-blog")
 		viper.AddConfigPath(App.RootDir + "/config")
+
+		//err := viper.AddSecureRemoteProvider("consul", "81.68.251.252:8500",
+		//	"config/s3", "315de223-fd51-a6ad-4c7e-2b61d1cf4416")
+		//
+		//if err = viper.ReadRemoteConfig(); err != nil {
+		//	panic(fmt.Errorf("Fatal error read remote config file: %s \n", err))
+		//}
 		//读取配置文件
 		if err := viper.ReadInConfig(); err != nil {
 			panic(fmt.Errorf("Fatal error config file: %s \n", err))
 		}
+		NacosInit()
+
 		//监控配置文件
 		viper.WatchConfig()
 
